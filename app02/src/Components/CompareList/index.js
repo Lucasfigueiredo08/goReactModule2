482d384f0e9d1467a4b2147ac506ff4f9305fbe8
Lucas Figueiredo <lukas.liberato14@gmail.com>
2020-06-15 18:14:01 -0300

    AULA 09 - Axios

--- app02/src/Components/CompareList/index.js
+++ app02/src/Components/CompareList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Repository } from './styles';
 
@@ -7,7 +8,8 @@ import { Container, Repository } from './styles';
 const CompareList = ({ repositories }) => (
   <Container>
     {repositories.map(repository => (
-      <Repository>
+      // id de cada repositorio
+      <Repository key={repository.id} >
           <header>
             <img src={repository.owner.avatar_url} alt={repository.owner.login} />
             <strong>{repository.name}</strong>
@@ -39,5 +41,21 @@ const CompareList = ({ repositories }) => (
 
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    owner: PropTypes.shape({
+      login: PropTypes.string,
+      avatar_url: PropTypes.string,
+    }),
+    stargazers_count: PropTypes.number,
+    forks_count: PropTypes.number,
+    open_issues_count: PropTypes.number,
+    pushed_at: PropTypes.string,
+  })).isRequired,
+
+}
 
 export default CompareList;
